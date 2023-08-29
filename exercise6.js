ffdd2fbe72c92f1492d818b423e3ae2ff3cd6f07
Luen67 <135562795+Luen67@users.noreply.github.com>
2023-08-28 22:12:16 -0600

    Exercise 6 (280824)

---
+++ exercise6.js
@@ -0,0 +1,31 @@
+/*Una funcion que cambie las minusculas por mayusculas de una cadena dada pero solo las letras en posición impar*/
+const convertUpperCaseLetterImpar = (string) => {
+    for(let index = 0; index < string.length; index++){
+        
+    }
+
+
+}
+
+
+
+/*función para invertir un string ingresado*/
+
+
+
+
+/*Funcipn que indica true si la palabra usa todas las vocales y false si no es asi*/
+
+
+
+/*crea una funcion llamada sayHi que recibe como parametro un nombre y te salude*/
+
+
+
+/*Crea una funcion llamada executor que reciba como primer parametro una funcion y como segundo 
+parametro un string y que en su cuerpo ejecute la funcion recibida y pase como parametro el segundo parametro recibido en esta funcion*/
+
+
+
+
+/*Ejecuta la funcion executor anterior pasando como primer parametro la funcion sayHi y como segundo parametro un string con tu nombre*/
