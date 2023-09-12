0b698b3292670903c52fa1149c087b8669a141eb
Luen67 <135562795+Luen67@users.noreply.github.com>
2023-09-12 12:45:30 -0600

    Add files via upload

--- Practice6/script.js
+++ Practice6/script.js
@@ -1,27 +1,29 @@
 const form = document.querySelector('#form');
+const avatar = document.querySelector('#avatar');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const fechaNacimiento = document.querySelector('#fecha_nacimiento');
+const genero = document.querySelectorAll('input[name="gender"]');
+const pais = document.querySelector('#pais');
+const descripcion = document.querySelector('#descripcion');
 const btnEnviar = document.querySelector('#btn_enviar');
 const cardContainer = document.querySelector('#card_container');
 const labelValidation = document.querySelector('#validation');
-
-const personas = {
-
-};
+let personas = [];
+let idPersona = 1;
+let persona = { id: 0};
 
 btnEnviar.addEventListener('click', () => {
-    createCard();
+    if(persona.id == 0){
+        createCard();
+    }else{
+        updateCard();
+    }
 });
 
 const createCard = () =>{
-    const avatar = document.querySelector('#avatar');
-    const nombre = document.querySelector('#nombre');
-    const apellido = document.querySelector('#apellido');
-    const fechaNacimiento = document.querySelector('#fecha_nacimiento');
-    const genero = document.querySelector('input[name="gender"]:checked');
-    const pais = document.querySelector('#pais');
-    const descripcion = document.querySelector('#descripcion');
 
-    if(avatar.value === "" || nombre.value === "" || apellido.value === "" || fechaNacimiento.value === "" ||
-    genero.value === ""){
+    if(!validaForm()){
         labelValidation.textContent = 'Los campos avatar, nombre, apellido, fecha y genero son obligatorios';
         return;
     }
@@ -30,7 +32,8 @@ const createCard = () =>{
     const divCard = document.createElement('div');
     divCard.className = 'card mt-5 d-flex flex-row';
     const img = document.createElement('img');
-    img.setAttribute('src',avatar.value);
+    img.setAttribute('src',avatar.value);   
+    img.setAttribute('id','avatar-'+idPersona);
     img.className = 'card-img-top img__width';
     const divCardBody = document.createElement('div');
     divCardBody.className = 'card-body row';
@@ -40,11 +43,13 @@ const createCard = () =>{
     const h5Apellido = document.createElement('h5');
     h5Apellido.className = 'card-text col-6';
     h5Apellido.textContent = 'Apellido';
-    const pNombre = document.createElement('p');
+    const pNombre = document.createElement('p');    
+    pNombre.setAttribute('id','nombre-'+idPersona);
     pNombre.textContent = nombre.value;
     pNombre.className = 'card-text col-6';
     const pApellido = document.createElement('p');
-    pApellido.textContent = apellido.value;
+    pApellido.textContent = apellido.value; 
+    pApellido.setAttribute('id','apellido-'+idPersona);
     pApellido.className = 'card-text col-6';
     const h5Fecha = document.createElement('h5');
     h5Fecha.className = 'card-text col-6';
@@ -52,19 +57,23 @@ const createCard = () =>{
     const h5Genero = document.createElement('h5');
     h5Genero.className = 'card-text col-6';
     h5Genero.textContent = 'Genero';
-    const pFecha = document.createElement('p');
+    const pFecha = document.createElement('p'); 
+    pFecha.setAttribute('id','fechaNacimiento-'+idPersona);
     pFecha.textContent = fechaNacimiento.value;
     pFecha.className = 'card-text col-6';
-    const pGenero = document.createElement('p');
+    const pGenero = document.createElement('p'); 
+    pGenero.setAttribute('id','genero-'+idPersona);
     pGenero.className = 'card-text col-6';
-    pGenero.textContent = genero.value;
+    pGenero.textContent = getRadioCheckedValue(genero);
     const buttonEditar = document.createElement('button');
-    buttonEditar.className = 'btn btn-primary btn-sm col-5';
+    buttonEditar.className = 'btn btn-primary btn-sm col-5 btn-edit-'+idPersona;
+    buttonEditar.setAttribute('id-persona',idPersona);
     buttonEditar.textContent = 'Editar'
     const divSpace = document.createElement('div');
     divSpace.className = 'col-2';
     const buttonEliminar = document.createElement('button');
-    buttonEliminar.className = 'btn btn-danger btn-sm col-5';
+    buttonEliminar.className = 'btn btn-danger btn-sm col-5 btn-delete-'+idPersona;
+    buttonEliminar.setAttribute('id-persona',idPersona);
     buttonEliminar.textContent = 'Eliminar';
     cardContainer.appendChild(divCard);
     divCard.appendChild(img);
@@ -81,17 +90,107 @@ const createCard = () =>{
     divCardBody.appendChild(divSpace);
     divCardBody.appendChild(buttonEliminar);
 
+    buttonEditar.addEventListener('click', (e) => {
+        editCard(e);
+    })
+
+    personas.push({
+        'id': idPersona,
+        'avatar': avatar.value,
+        'nombre': nombre.value,
+        'apellido': apellido.value,
+        'fechaNacimiento': fechaNacimiento.value,
+        'genero': getRadioCheckedValue(genero),
+        'pais': pais.value,
+        'descripcion': descripcion.value
+    })
+
+    idPersona++;
+
     form.reset();
 };
 
-const findPersona = (persona) => {
-
-}
-
-const editCard = () => {
-
+const editCard = (e) => {
+    const id = e.currentTarget.getAttribute('id-persona');
+    e.currentTarget.textContent = 'Cancelar';
+    persona = personas.filter((item) => item.id == id )[0];
+    avatar.value = persona.avatar;
+    nombre.value = persona.nombre;
+    apellido.value = persona.apellido;
+    fechaNacimiento.value = persona.fechaNacimiento;
+    setRadioCheckedValue(genero,persona.genero);
+    pais.value = persona.pais;
+    descripcion.value = persona.descripcion;
+    labelValidation.textContent = "";
+    e.currentTarget.addEventListener('click', (e) => {
+        cancelEditCard(e);
+    })
 }
 
 const updateCard = () => {
+    if(!validaForm()){
+        labelValidation.textContent = 'Los campos avatar, nombre, apellido, fecha y genero son obligatorios';
+        return;
+    }
+
+    labelValidation.textContent = "";
+    const index = personas.indexOf(persona);
+    const buttonEditar = document.querySelector('.btn-edit-'+persona.id)
+    document.querySelector('#avatar-'+persona.id).textContent = avatar.value;
+    document.querySelector('#nombre-'+persona.id).textContent = nombre.value;
+    document.querySelector('#apellido-'+persona.id).textContent = apellido.value;
+    document.querySelector('#fechaNacimiento-'+persona.id).textContent = fechaNacimiento.value;
+    document.querySelector('#genero-'+persona.id).textContent = getRadioCheckedValue(genero);
     
+    persona.avatar = avatar.value;
+    persona.nombre = nombre.value;
+    persona.apellido = apellido.value;
+    persona.fechaNacimiento = fechaNacimiento.value;
+    persona.genero = getRadioCheckedValue(genero);
+    persona.pais = pais.value;
+    persona.descripcion = descripcion.value;
+
+    personas.splice(index, 1, persona);
+
+    buttonEditar.textContent = 'Editar';
+    buttonEditar.addEventListener('click', (e) => {
+        editCard(e);
+    })
+    persona = { id: 0};
+    form.reset();
+}
+
+const cancelEditCard = (e) => {
+    e.currentTarget.textContent = 'Editar';
+    e.currentTarget.addEventListener('click', (e) => {
+        editCard(e);
+    })
+    persona = { id: 0};
+    form.reset();
+}
+
+const getRadioCheckedValue = (radioButtons) => {
+    let value = "";
+    radioButtons.forEach(function(radioButton) {
+        if (radioButton.checked) {
+          value = radioButton.value;
+        }
+      });
+      return value;
+}
+
+const setRadioCheckedValue = (radioButtons, value) => {    
+    radioButtons.forEach(function(radioButton) {
+        if (radioButton.value == value) {
+            radioButton.checked = true;
+        }
+      });
+}
+
+const validaForm = () =>{
+    if(avatar.value === "" || nombre.value === "" || apellido.value === "" || fechaNacimiento.value === "" ||
+        genero.value === ""){
+        return false;
+    }
+    return true;
 }
